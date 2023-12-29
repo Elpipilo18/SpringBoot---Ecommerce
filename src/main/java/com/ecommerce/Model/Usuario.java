2d633316d837cdf391697c2d21f3a73e6422c8e6
felipe <ocampoaraujofelipe@gmail.com>
2023-12-29 16:57:34 -0600

    mapeo de la base de datos

--- src/main/java/com/ecommerce/Model/Usuario.java
+++ src/main/java/com/ecommerce/Model/Usuario.java
@@ -1,7 +1,22 @@
 package com.ecommerce.Model;
 
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	@Column
 	private String nombre;
 	private String username;
 	private String email;
@@ -10,6 +25,10 @@ public class Usuario {
 	private String tipo;
 	private String password;
 	
+	@OneToMany(mappedBy = "usuario")
+	private List<Producto> productos;
+	@OneToMany(mappedBy = "usuario")
+	private List<Orden> ordenes;
 	
 	public Usuario() {
 		// TODO Auto-generated constructor stub
@@ -78,6 +97,28 @@ public class Usuario {
 	}
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	
+
+	public List<Producto> getProductos() {
+		return productos;
+	}
+
+
+	public void setProductos(List<Producto> productos) {
+		this.productos = productos;
+	}
+	
+	
+
+	public List<Orden> getOrdenes() {
+		return ordenes;
+	}
+
+
+	public void setOrdenes(List<Orden> ordenes) {
+		this.ordenes = ordenes;
 	}
 
 
