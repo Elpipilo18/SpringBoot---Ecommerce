e35c67c4586a7a9d480d3e092bc4d52b9eaeacb1
felipe <ocampoaraujofelipe@gmail.com>
2023-12-30 20:42:27 -0600

    funcionalidad para eliminar un producto agregada

--- src/main/java/com/ecommerce/Controller/ProductoController.java
+++ src/main/java/com/ecommerce/Controller/ProductoController.java
@@ -6,6 +6,7 @@ import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -61,6 +62,11 @@ public class ProductoController {
 	@PostMapping("actualizar")
 	public String update(Producto producto) {
 		productoService.update(producto);
+		return "redirect:/productos";
+	}
+	@GetMapping("eliminar/{id}")
+	public String delete(@PathVariable Integer id) {
+		productoService.delete(id);
 		return "redirect:/productos";
 	}
 }
