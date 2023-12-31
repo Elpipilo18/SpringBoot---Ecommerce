af034162e49d1053846356bbfd2008f534945afa
felipe <ocampoaraujofelipe@gmail.com>
2023-12-30 23:31:37 -0600

    servicio de subir imagen al servidor

--- src/main/java/com/ecommerce/Repository/ProductoRepository.java
+++ src/main/java/com/ecommerce/Repository/ProductoRepository.java
@@ -1,11 +1,9 @@
 package com.ecommerce.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.ecommerce.Model.Producto;
 
-@Repository
 public interface ProductoRepository extends JpaRepository<Producto, Integer>{
 
 }
---
+++ src/main/java/com/ecommerce/Service/UploadFileService.java
@@ -0,0 +1,27 @@
+package com.ecommerce.Service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class UploadFileService {
+	
+	private String folder = "images//";
+	
+	public String saveImage(MultipartFile file) throws IOException {
+		if(!file.isEmpty()) {
+			byte [] bytes = file.getBytes();
+			Path path = Paths.get(folder + file.getOriginalFilename());
+			Files.write(path, bytes);
+			
+			return file.getOriginalFilename();
+		}
+		
+		return "default.jpg";
+	}
+}
